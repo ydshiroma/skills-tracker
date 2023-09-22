b4e1375d27aef76b242e050cc061c436d114dada
Yumi <ydshiroma@gmail.com>
2023-09-21 21:26:40 -0400

    stopped before bonus missions

---
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -0,0 +1,67 @@
+package org.launchcode.skillstracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class SkillsController {
+    @GetMapping("/")
+    @ResponseBody
+    public String renderHomePage() {
+        return "<h1>Skills Tracker</h1>" +
+                "<h2>We have a few skills we would like you to learn. Here is the list: </h2>" +
+                "<ol>" +
+                "<li>Java</li>" +
+                "<li>JavaScript</li>" +
+                "<li>Python</li>" +
+                "</ol>";
+    }
+
+    @GetMapping("/form")
+    @ResponseBody
+    public String manageUserInput() {
+        return "<form action='/form' method='post'>" +
+                "<label for='name'>Name: </label>" +
+                "<input type='text' id='name' name='name'>" +
+                "</label>" +
+                "<br>" +
+                "<label for='language1'>My favorite language:</label>" +
+                "<br>" +
+                "<select name='language1' id='language1'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='Javascript'>JavaScript</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select>" +
+                "<br>" +
+                "<label for='language2'>My second favorite language:</label>" +
+                "<br>" +
+                "<select name='language2' id='language2'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='Javascript'>JavaScript</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select>" +
+                "<br>" +
+                "<label for='language3'>My third favorite language:</label>" +
+                "<br>" +
+                "<select name='language3' id='language3'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select>" +
+                "<br>" +
+                "<input type='submit' value='Submit'>" +
+                "</form>";
+    }
+
+//    @RequestMapping(value = "/form", method = {RequestMethod.POST})
+    @PostMapping("/form")
+    @ResponseBody
+    public String displayUserData(@RequestParam String name, String language1, String language2, String language3) {
+        return "<h1>" + name + "</h1>" +
+                "<ol>" +
+                "<li>" + language1 + "</li>" +
+                "<li>" + language2 + "</li>" +
+                "<li>" + language3 + "</li>" +
+                "</ol>";
+    }
+}
